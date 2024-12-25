70375fd28bfbcf10a74532d2282ee4d9dde36a14
Dina Sokol <dsokol@brooklyn.cuny.edu>
2024-12-25 13:53:17 -0500

    first commit

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int num{ 5 };
+    num = 10;
+    double num2{ 5.5 };
+    bool isTrue{ true };
+    char ch{ 'A' };
+
+    std::cout << "Value of num is: " << num << " num2: " << num2 << " ch: " << ch << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
