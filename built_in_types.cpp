41d0c208d3d2dac3d97281a2ee80bb9df8c98838
dsokol101 <sokol.dina@gmail.com>
2025-01-30 10:31:46 -0500

    added some C++ specific details about types

--- built_in_types.cpp
+++ built_in_types.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
-
+#include <cstdint>
+/** 1. brace initialization
+ *  2. there are many specific types used in C++
+ *  3. you can use auto and allow the compiler to deduce the type
+ */
 int main() {
     int num{ 5 };
-    num = 10;
+    auto num3 = 10;
     double num2{ 5.5 };
     bool isTrue{ true };
     char ch{ 'A' };
+    int16_t x;
+    int64_t y;
+    size_t sz;
 
     std::cout << "Value of num is: " << num << " num2: " << num2 << " ch: " << ch << std::endl;
-    std::cout << "bool prints as an int: " << isTrue << std::endl;
+    std::cout << "bool prints as an int: " << isTrue << " char ch: "
+              << ch <<  std::endl;
     return 0;
 }
