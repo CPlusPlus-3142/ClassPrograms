ba4a9bdae75af28b1b8e6d23db8507a71e71bee7
dsokol101 <sokol.dina@gmail.com>
2025-02-04 09:47:43 -0500

    declaration vs definition

--- built_in_types.cpp
+++ built_in_types.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <cstdint>
+using namespace std;
+
 /** 1. brace initialization
  *  2. there are many specific types used in C++
  *  3. you can use auto and allow the compiler to deduce the type
  */
+void simple(int); // function declaration or prototype
+
 int main() {
     int num{ 5 };
     auto num3 = 10;
@@ -14,8 +18,20 @@ int main() {
     int64_t y;
     size_t sz;
 
+
+    extern int size; // somewhere else I defined an int var size
+    // and this is only a declaration
+    size=10;
+    int arr[size];
+
     std::cout << "Value of num is: " << num << " num2: " << num2 << " ch: " << ch << std::endl;
     std::cout << "bool prints as an int: " << isTrue << " char ch: "
               << ch <<  std::endl;
+    cout << "# of bytes in a char: " << sizeof(char) << std::endl;
+    simple(5);
     return 0;
+}
+// function definition
+void simple(int x) {
+    std::cout << x;
 }
