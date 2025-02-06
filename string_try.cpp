6ace255c5fc64848da8513f949bb9a6df71061d2
Dina Sokol <dsokol@brooklyn.cuny.edu>
2025-02-06 18:01:02 -0500

    2/6 inclass additions

--- string_try.cpp
+++ string_try.cpp
@@ -6,9 +6,12 @@ using namespace std;
 void printString(string str) {
   cout << " str's length: " << str.length() << endl;
   cout << " str's first character: " << str[0] << endl;
-  cout << " str's last character: " << str.at(str.length()+2) << endl;
+  cout << " str's last character: " << str.at(str.length()-1) << endl;
 
   string str2="apple", str3="banana";
+  string str4 = str3;
+  str4.append("s");
+  cout << "str4: " << str4 << " str3: " << str3 << endl;
   if (str3<str2) {
     cout << "banana comes before apple" << endl;
   } else {
